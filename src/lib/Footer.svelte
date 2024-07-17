23f5571fb86b8a0f4d4e4097c1951fc6d93e34f3
ShantanuK86 <sk9411011994@gmail.com>
2024-07-17 20:23:15 +0530

    some changes

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,7 @@
+<div class="w-screen bottom-0 absolute h-[100px] bg-blue-200">
+    <div class="flex items-center justify-center p-5">
+    
+        <p class="italic font-mono font-bold">SVELTE WORKSHOP</p>
+       
+    </div>
+    </div>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,7 @@
+<div class="max-w-[100vw] h-[100px] bg-blue-200">
+<div class="flex justify-between items-center p-5">
+
+    <p class="italic font-mono font-bold">Timechain summer of Code '24</p>
+    <p class="font-extrabold font-serif">Shantanu Kumar </p>
+</div>
+</div>
